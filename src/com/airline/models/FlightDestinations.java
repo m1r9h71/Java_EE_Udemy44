50df7355bb3e5df06b013056f0596a83b8ce9f95
m1r9h71 <matthoing@icloud.com>
2018-07-23 13:17:02 +0100

    Adding new JPA entity classes for flight and airplane

---
+++ src/com/airline/models/FlightDestinations.java
@@ -0,0 +1,15 @@
+package com.airline.models;
+
+public enum FlightDestinations {
+	
+	San_Francisco,
+	Los_Angeles,
+	New_York,
+	London_Gatwick,
+	Paris,
+	Rome,
+	Amsterdam,
+	Waterford,
+	Dublin
+
+}
