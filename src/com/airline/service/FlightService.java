e7c50d1003da51fd2cefb49060c79fc2ae221466
m1r9h71 <matthoing@icloud.com>
2018-07-24 16:04:02 +0100

    Using named queries to query by id

--- src/com/airline/service/FlightService.java
+++ src/com/airline/service/FlightService.java
@@ -4,9 +4,11 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import com.airline.models.Airplane;
 import com.airline.models.Flight;
+import com.airline.models.Pilot;
 
 /**
  * Session Bean implementation class FlightService
@@ -28,6 +30,23 @@ public class FlightService {
     public void addFlight(Flight f, Airplane a) {
     	em.persist(f);
     	em.persist(a);
+    	
+    	
+    }
+    
+    public void addPilotToFlight(String pilotId, String flightId) {
+    	
+    	TypedQuery<Flight> fQuery = em.createNamedQuery("Flight.findById", Flight.class);
+    	
+    	fQuery.setParameter("id", Integer.parseInt(flightId));
+    	
+    	Flight f = fQuery.getSingleResult();
+    	
+    	TypedQuery<Pilot> pQuery = em.createNamedQuery("Pilot.findById", Pilot.class);
+    	
+    	pQuery.setParameter("id", Integer.parseInt(pilotId));
+    	
+    	Pilot p = pQuery.getSingleResult();
     	
     	
     }
